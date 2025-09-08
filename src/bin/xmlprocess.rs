c5f3068dc1d0180beca7195ca28a3b29892cb4ae
tayu0110 <tayu.dev@gmail.com>
2025-09-08 23:52:53 +0900

    Implement `xmlprocess` command and add `DebugHandler`.

---
+++ src/bin/xmlprocess.rs
@@ -0,0 +1,43 @@
+use std::path::PathBuf;
+
+use anyxml::{
+    error::XMLError,
+    sax::{
+        handler::DebugHandler,
+        parser::{ParserOption, XMLReaderBuilder},
+    },
+};
+use anyxml_uri::uri::URIString;
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct CmdArgs {
+    #[clap(long, help = "inspect with SAX API")]
+    sax: bool,
+    #[clap(long, help = "validate using DTD")]
+    dtd_valid: bool,
+    #[clap(long, help = "disable namespace handling")]
+    no_namespace: bool,
+    #[clap(help = "path to the target XML document")]
+    file: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = CmdArgs::parse();
+
+    let mut builder = XMLReaderBuilder::new().set_handler(DebugHandler::default());
+    if args.dtd_valid {
+        builder = builder.enable_option(ParserOption::Validation);
+    }
+    if args.no_namespace {
+        builder = builder.disable_option(ParserOption::Namespaces);
+    }
+    let mut reader = builder.build();
+    let uri = URIString::parse_file_path(args.file).map_err(XMLError::from)?;
+    reader.parse_uri(uri, None)?;
+    if args.sax {
+        print!("{}", reader.handler.buffer);
+    }
+
+    Ok(())
+}
--- src/sax/handler.rs
+++ src/sax/handler.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, sync::Arc};
+use std::{fmt::Write as _, fs::File, sync::Arc};
 
 use anyxml_uri::uri::URIStr;
 
@@ -231,3 +231,239 @@ impl SAXHandler for DefaultSAXHandler {
     }
 }
 impl EntityResolver for DefaultSAXHandler {}
+
+pub struct DebugHandler<Child: SAXHandler = DefaultSAXHandler> {
+    pub buffer: String,
+    pub child: Child,
+}
+
+impl EntityResolver for DebugHandler {
+    fn get_external_subset(
+        &mut self,
+        name: &str,
+        base_uri: Option<&URIStr>,
+    ) -> Result<InputSource<'static>, XMLError> {
+        writeln!(
+            self.buffer,
+            "getExternalSubset({name}, {:?})",
+            base_uri.map(|uri| uri.as_escaped_str())
+        )
+        .ok();
+        self.child.get_external_subset(name, base_uri)
+    }
+
+    fn resolve_entity(
+        &mut self,
+        name: &str,
+        public_id: Option<&str>,
+        base_uri: &URIStr,
+        system_id: &URIStr,
+    ) -> Result<InputSource<'static>, XMLError> {
+        writeln!(
+            self.buffer,
+            "resolveEntity({name}, {public_id:?}, {}, {})",
+            base_uri.as_escaped_str(),
+            system_id.as_escaped_str()
+        )
+        .ok();
+        self.child
+            .resolve_entity(name, public_id, base_uri, system_id)
+    }
+}
+
+impl SAXHandler for DebugHandler {
+    fn characters(&mut self, data: &str) {
+        writeln!(self.buffer, "characters({data})").ok();
+        self.child.characters(data);
+    }
+
+    fn declaration(&mut self, version: &str, encoding: Option<&str>, standalone: Option<bool>) {
+        writeln!(
+            self.buffer,
+            "declaration({version}, {encoding:?}, {standalone:?})"
+        )
+        .ok();
+        self.child.declaration(version, encoding, standalone);
+    }
+
+    fn ignorable_whitespace(&mut self, data: &str) {
+        writeln!(self.buffer, "ignorableWhitespace({data})").ok();
+        self.child.ignorable_whitespace(data);
+    }
+
+    fn processing_instruction(&mut self, target: &str, data: Option<&str>) {
+        writeln!(self.buffer, "processingInstruction({target}, {data:?})").ok();
+        self.child.processing_instruction(target, data);
+    }
+
+    fn set_document_locator(&mut self, locator: Arc<Locator>) {
+        writeln!(self.buffer, "setDocumentLocator()").ok();
+        self.child.set_document_locator(locator);
+    }
+
+    fn skipped_entity(&mut self, name: &str) {
+        writeln!(self.buffer, "skippedEntity({name})").ok();
+        self.child.skipped_entity(name);
+    }
+
+    fn start_document(&mut self) {
+        writeln!(self.buffer, "startDocument()").ok();
+        self.child.start_document();
+    }
+    fn end_document(&mut self) {
+        writeln!(self.buffer, "endDocument()").ok();
+        self.child.end_document();
+    }
+
+    fn start_element(
+        &mut self,
+        uri: Option<&str>,
+        local_name: Option<&str>,
+        qname: &str,
+        atts: &Attributes,
+    ) {
+        write!(self.buffer, "startElement({uri:?}, {local_name:?}, {qname}").ok();
+        for att in atts {
+            write!(self.buffer, ", ").ok();
+            if let Some(local_name) = att.local_name.as_deref() {
+                write!(self.buffer, "{{{:?}}}{local_name}='{}'", att.uri, att.value).ok();
+            } else {
+                write!(self.buffer, "{}='{}'", att.qname, att.value).ok();
+            }
+        }
+        writeln!(self.buffer, ")").ok();
+        self.child.start_element(uri, local_name, qname, atts);
+    }
+    fn end_element(&mut self, uri: Option<&str>, local_name: Option<&str>, qname: &str) {
+        writeln!(self.buffer, "endElement({uri:?}, {local_name:?}, {qname})").ok();
+        self.child.end_element(uri, local_name, qname);
+    }
+
+    fn start_prefix_mapping(&mut self, prefix: Option<&str>, uri: &str) {
+        writeln!(self.buffer, "startPrefixMapping({prefix:?}, {uri})").ok();
+        self.child.start_prefix_mapping(prefix, uri);
+    }
+    fn end_prefix_mapping(&mut self, prefix: Option<&str>) {
+        writeln!(self.buffer, "endPrefixMapping({prefix:?})").ok();
+        self.child.end_prefix_mapping(prefix);
+    }
+
+    fn attribute_decl(
+        &mut self,
+        element_name: &str,
+        attribute_name: &str,
+        attribute_type: &AttributeType,
+        default_decl: &DefaultDecl,
+    ) {
+        writeln!(
+            self.buffer,
+            "attributeDecl({element_name}, {attribute_name}, {attribute_type:?}, {default_decl:?})"
+        )
+        .ok();
+        self.child
+            .attribute_decl(element_name, attribute_name, attribute_type, default_decl);
+    }
+
+    fn element_decl(&mut self, name: &str, contentspec: &ContentSpec) {
+        writeln!(self.buffer, "elementDecl({name}, {contentspec:?})").ok();
+        self.child.element_decl(name, contentspec);
+    }
+
+    fn external_entity_decl(&mut self, name: &str, public_id: Option<&str>, system_id: &URIStr) {
+        writeln!(
+            self.buffer,
+            "externalEntityDecl({name}, {public_id:?}, {system_id:?})"
+        )
+        .ok();
+        self.child.external_entity_decl(name, public_id, system_id);
+    }
+
+    fn internal_entity_decl(&mut self, name: &str, value: &str) {
+        writeln!(self.buffer, "internalEntityDecl({name}, {value})").ok();
+        self.child.internal_entity_decl(name, value);
+    }
+
+    fn notation_decl(&mut self, name: &str, public_id: Option<&str>, system_id: Option<&URIStr>) {
+        writeln!(
+            self.buffer,
+            "notationDecl({name}, {public_id:?}, {system_id:?})"
+        )
+        .ok();
+        self.child.notation_decl(name, public_id, system_id);
+    }
+
+    fn unparsed_entity_decl(
+        &mut self,
+        name: &str,
+        public_id: Option<&str>,
+        system_id: &URIStr,
+        notation_name: &str,
+    ) {
+        writeln!(
+            self.buffer,
+            "unparsedEntityDecl({name}, {public_id:?}, {}, {notation_name})",
+            system_id.as_escaped_str()
+        )
+        .ok();
+        self.child
+            .unparsed_entity_decl(name, public_id, system_id, notation_name);
+    }
+
+    fn error(&mut self, error: SAXParseError) {
+        self.child.error(error);
+    }
+
+    fn fatal_error(&mut self, error: SAXParseError) {
+        self.child.fatal_error(error);
+    }
+
+    fn warning(&mut self, error: SAXParseError) {
+        self.child.warning(error);
+    }
+
+    fn comment(&mut self, data: &str) {
+        writeln!(self.buffer, "comment({data})").ok();
+        self.child.comment(data);
+    }
+
+    fn start_cdata(&mut self) {
+        writeln!(self.buffer, "startCDATA()").ok();
+        self.child.start_cdata();
+    }
+    fn end_cdata(&mut self) {
+        writeln!(self.buffer, "endCDATA()").ok();
+        self.child.end_cdata();
+    }
+
+    fn start_dtd(&mut self, name: &str, public_id: Option<&str>, system_id: Option<&URIStr>) {
+        writeln!(
+            self.buffer,
+            "startDTD({name}, {public_id:?}, {:?})",
+            system_id.map(|id| id.as_escaped_str())
+        )
+        .ok();
+        self.child.start_dtd(name, public_id, system_id);
+    }
+    fn end_dtd(&mut self) {
+        writeln!(self.buffer, "endDTD()").ok();
+        self.child.end_dtd();
+    }
+
+    fn start_entity(&mut self, name: &str) {
+        writeln!(self.buffer, "startEntity({name})").ok();
+        self.child.start_entity(name);
+    }
+    fn end_entity(&mut self) {
+        writeln!(self.buffer, "endEntity()").ok();
+        self.child.end_entity();
+    }
+}
+
+impl Default for DebugHandler {
+    fn default() -> Self {
+        Self {
+            buffer: String::new(),
+            child: DefaultSAXHandler,
+        }
+    }
+}
