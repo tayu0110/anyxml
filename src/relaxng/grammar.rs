a7bc7118f7b12afd8eb46b7c20c67c9caaa91737
tayu0110 <tayu.dev@gmail.com>
2026-02-11 20:33:17 +0900

    Remove `TryFrom<Element>` implementation for RELAX NG schema types.

--- src/relaxng/grammar.rs
+++ src/relaxng/grammar.rs
@@ -7,7 +7,6 @@ use crate::{
         parse::{RelaxNGNodeType, RelaxNGParseHandler},
     },
     sax::handler::SAXHandler,
-    tree::Element,
     uri::{URIStr, URIString},
 };
 
@@ -86,53 +85,6 @@ impl RelaxNGGrammar {
     }
 }
 
-impl TryFrom<Element> for RelaxNGGrammar {
-    type Error = XMLError;
-
-    fn try_from(grammar: Element) -> Result<Self, Self::Error> {
-        if grammar.local_name().as_ref() != "grammar" {
-            return Err(XMLError::RngParseUnknownError);
-        }
-
-        let mut start = None;
-        let mut define = BTreeMap::new();
-        let mut children = grammar.first_element_child();
-        while let Some(element) = children {
-            children = element.next_element_sibling();
-
-            match element.local_name().as_ref() {
-                "start" => {
-                    let top = element
-                        .first_element_child()
-                        .ok_or(XMLError::RngParseUnknownError)?;
-                    if top.local_name().as_ref() != "notAllowed" {
-                        start = Some(RelaxNGPattern::try_from(top)?);
-                    }
-                }
-                "define" => {
-                    let name = element
-                        .get_attribute("name", None)
-                        .ok_or(XMLError::RngParseUnknownError)?;
-                    define.insert(name, RelaxNGDefine::try_from(element)?);
-                }
-                _ => return Err(XMLError::RngParseUnknownError),
-            }
-        }
-
-        let grammar = Self {
-            start,
-            define,
-            libraries: RelaxNGDatatypeLibraries::default(),
-        };
-        grammar.verify_content_type()?;
-        grammar.verify_attribute_uniqueness()?;
-        grammar.verify_attribute_repeat()?;
-        grammar.verify_element_name_uniqueness()?;
-
-        Ok(grammar)
-    }
-}
-
 impl std::fmt::Display for RelaxNGGrammar {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -206,49 +158,6 @@ impl RelaxNGDefine {
     }
 }
 
-impl TryFrom<Element> for RelaxNGDefine {
-    type Error = XMLError;
-
-    fn try_from(define: Element) -> Result<Self, Self::Error> {
-        let element = define
-            .first_element_child()
-            .filter(|elem| elem.local_name().as_ref() == "element")
-            .ok_or(XMLError::RngParseUnknownError)?;
-
-        let name = define
-            .get_attribute("name", None)
-            .ok_or(XMLError::RngParseUnknownError)?
-            .into();
-        let name_class = element
-            .first_element_child()
-            .ok_or(XMLError::RngParseUnknownError)?;
-        let top = element
-            .last_element_child()
-            .ok_or(XMLError::RngParseUnknownError)?;
-
-        if name_class
-            .next_sibling()
-            .is_none_or(|next| !top.is_same_node(next))
-        {
-            return Err(XMLError::RngParseUnknownError);
-        }
-
-        if top.local_name().as_ref() == "notAllowed" {
-            Ok(Self {
-                name,
-                name_class: name_class.try_into()?,
-                top: None,
-            })
-        } else {
-            Ok(Self {
-                name,
-                name_class: name_class.try_into()?,
-                top: Some(top.try_into()?),
-            })
-        }
-    }
-}
-
 impl std::fmt::Display for RelaxNGDefine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -316,19 +225,6 @@ impl RelaxNGPattern {
             pattern.verify_element_name_uniqueness(grammar, name_classes, text_count)
         } else {
             Ok(())
-        }
-    }
-}
-
-impl TryFrom<Element> for RelaxNGPattern {
-    type Error = XMLError;
-
-    fn try_from(value: Element) -> Result<Self, Self::Error> {
-        match value.local_name().as_ref() {
-            "empty" => Ok(Self { pattern: None }),
-            _ => Ok(Self {
-                pattern: Some(value.try_into()?),
-            }),
         }
     }
 }
@@ -580,168 +476,6 @@ impl RelaxNGNonEmptyPattern {
     }
 }
 
-impl TryFrom<Element> for RelaxNGNonEmptyPattern {
-    type Error = XMLError;
-
-    fn try_from(value: Element) -> Result<Self, Self::Error> {
-        match value.local_name().as_ref() {
-            "text" => Ok(Self::Text),
-            "data" => {
-                let mut param = vec![];
-                let mut children = value.first_child();
-                while let Some(child) = children.as_ref() {
-                    let ch = child.as_element().ok_or(XMLError::RngParseUnknownError)?;
-                    match ch.local_name().as_ref() {
-                        "param" => param.push(ch.try_into()?),
-                        "except" => break,
-                        _ => return Err(XMLError::RngParseUnknownError),
-                    }
-
-                    children = child.next_sibling();
-                }
-
-                let mut except_pattern = None;
-                if let Some(except) = children
-                    .as_ref()
-                    .and_then(|ch| ch.as_element())
-                    .filter(|ch| ch.local_name().as_ref() == "except")
-                {
-                    children = except.next_sibling();
-                    except_pattern = Some(Box::new(except.try_into()?));
-                }
-                if children.is_some() {
-                    return Err(XMLError::RngParseUnknownError);
-                }
-
-                Ok(Self::Data {
-                    type_name: value
-                        .get_attribute("type", None)
-                        .ok_or(XMLError::RngParseUnknownError)?
-                        .into(),
-                    datatype_library: URIString::parse(
-                        value
-                            .get_attribute("datatypeLibrary", None)
-                            .ok_or(XMLError::RngParseUnknownError)?,
-                    )?
-                    .into(),
-                    param,
-                    except_pattern,
-                })
-            }
-            "value" => Ok(Self::Value {
-                type_name: value
-                    .get_attribute("type", None)
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .into(),
-                datatype_library: URIString::parse(
-                    value
-                        .get_attribute("datatypeLibrary", None)
-                        .ok_or(XMLError::RngParseUnknownError)?,
-                )?
-                .into(),
-                ns: value
-                    .get_attribute("ns", None)
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .into(),
-                value: value
-                    .first_child()
-                    .map(|ch| ch.text_content())
-                    .unwrap_or_default()
-                    .into(),
-            }),
-            "list" => Ok(Self::List {
-                pattern: Box::new(
-                    value
-                        .first_child()
-                        .and_then(|ch| ch.as_element())
-                        .ok_or(XMLError::RngParseUnknownError)?
-                        .try_into()?,
-                ),
-            }),
-            "attribute" => Ok(Self::Attribute {
-                name_class: value
-                    .first_element_child()
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .try_into()?,
-                pattern: Box::new(
-                    value
-                        .last_element_child()
-                        .ok_or(XMLError::RngParseUnknownError)?
-                        .try_into()?,
-                ),
-            }),
-            "ref" => Ok(Self::Ref {
-                name: value
-                    .get_attribute("name", None)
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .into(),
-            }),
-            "oneOrMore" => Ok(Self::OneOrMore {
-                pattern: Box::new(
-                    value
-                        .first_child()
-                        .and_then(|ch| ch.as_element())
-                        .ok_or(XMLError::RngParseUnknownError)?
-                        .try_into()?,
-                ),
-            }),
-            "choice" => Ok(Self::Choice {
-                left: Box::new(
-                    value
-                        .first_child()
-                        .and_then(|ch| ch.as_element())
-                        .ok_or(XMLError::RngParseUnknownError)?
-                        .try_into()?,
-                ),
-                right: Box::new(
-                    value
-                        .last_child()
-                        .and_then(|ch| ch.as_element())
-                        .ok_or(XMLError::RngParseUnknownError)?
-                        .try_into()?,
-                ),
-            }),
-            "group" => Ok(Self::Group {
-                pattern: [
-                    Box::new(
-                        value
-                            .first_child()
-                            .and_then(|ch| ch.as_element())
-                            .ok_or(XMLError::RngParseUnknownError)?
-                            .try_into()?,
-                    ),
-                    Box::new(
-                        value
-                            .last_child()
-                            .and_then(|ch| ch.as_element())
-                            .ok_or(XMLError::RngParseUnknownError)?
-                            .try_into()?,
-                    ),
-                ],
-            }),
-            "interleave" => Ok(Self::Interleave {
-                pattern: [
-                    Box::new(
-                        value
-                            .first_child()
-                            .and_then(|ch| ch.as_element())
-                            .ok_or(XMLError::RngParseUnknownError)?
-                            .try_into()?,
-                    ),
-                    Box::new(
-                        value
-                            .last_child()
-                            .and_then(|ch| ch.as_element())
-                            .ok_or(XMLError::RngParseUnknownError)?
-                            .try_into()?,
-                    ),
-                ],
-            }),
-            _ => Err(XMLError::RngParseUnknownError),
-        }
-    }
-}
-
 impl std::fmt::Display for RelaxNGNonEmptyPattern {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -806,28 +540,6 @@ pub(super) struct RelaxNGParam {
     pub(super) value: Box<str>,
 }
 
-impl TryFrom<Element> for RelaxNGParam {
-    type Error = XMLError;
-
-    fn try_from(value: Element) -> Result<Self, Self::Error> {
-        if value.local_name().as_ref() != "param" {
-            return Err(XMLError::RngParseUnknownError);
-        }
-
-        Ok(Self {
-            name: value
-                .get_attribute("name", None)
-                .ok_or(XMLError::RngParseUnknownError)?
-                .into(),
-            value: value
-                .first_child()
-                .map(|ch| ch.text_content())
-                .ok_or(XMLError::RngParseUnknownError)?
-                .into(),
-        })
-    }
-}
-
 impl std::fmt::Display for RelaxNGParam {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "<param name=\"{}\">{}</param>", self.name, self.value)
@@ -836,24 +548,6 @@ impl std::fmt::Display for RelaxNGParam {
 
 pub(super) struct RelaxNGExceptPattern {
     pub(super) pattern: RelaxNGPattern,
-}
-
-impl TryFrom<Element> for RelaxNGExceptPattern {
-    type Error = XMLError;
-
-    fn try_from(value: Element) -> Result<Self, Self::Error> {
-        if value.local_name().as_ref() != "except" {
-            return Err(XMLError::RngParseUnknownError);
-        }
-
-        Ok(Self {
-            pattern: value
-                .first_child()
-                .and_then(|ch| ch.as_element())
-                .ok_or(XMLError::RngParseUnknownError)?
-                .try_into()?,
-        })
-    }
 }
 
 impl std::fmt::Display for RelaxNGExceptPattern {
@@ -973,63 +667,6 @@ impl RelaxNGNameClass {
     }
 }
 
-impl TryFrom<Element> for RelaxNGNameClass {
-    type Error = XMLError;
-
-    fn try_from(value: Element) -> Result<Self, Self::Error> {
-        match value.local_name().as_ref() {
-            "anyName" => Ok(Self::AnyName {
-                except: value
-                    .first_child()
-                    .and_then(|ch| ch.as_element())
-                    .map(RelaxNGExceptNameClass::try_from)
-                    .transpose()?,
-            }),
-            "nsName" => Ok(Self::NsName {
-                ns: value
-                    .get_attribute("ns", None)
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .into(),
-                except: value
-                    .first_child()
-                    .and_then(|ch| ch.as_element())
-                    .map(RelaxNGExceptNameClass::try_from)
-                    .transpose()?,
-            }),
-            "name" => Ok(Self::Name {
-                ns: value
-                    .get_attribute("ns", None)
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .into(),
-                value: value
-                    .first_child()
-                    .map(|ch| ch.text_content())
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .into(),
-            }),
-            "choice" => Ok(Self::Choice {
-                name_class: [
-                    Box::new(
-                        value
-                            .first_child()
-                            .and_then(|ch| ch.as_element())
-                            .ok_or(XMLError::RngParseUnknownError)?
-                            .try_into()?,
-                    ),
-                    Box::new(
-                        value
-                            .last_child()
-                            .and_then(|ch| ch.as_element())
-                            .ok_or(XMLError::RngParseUnknownError)?
-                            .try_into()?,
-                    ),
-                ],
-            }),
-            _ => Err(XMLError::RngParseUnknownError),
-        }
-    }
-}
-
 impl std::fmt::Display for RelaxNGNameClass {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -1057,26 +694,6 @@ impl std::fmt::Display for RelaxNGNameClass {
 
 pub(super) struct RelaxNGExceptNameClass {
     name_class: Box<RelaxNGNameClass>,
-}
-
-impl TryFrom<Element> for RelaxNGExceptNameClass {
-    type Error = XMLError;
-
-    fn try_from(value: Element) -> Result<Self, Self::Error> {
-        if value.local_name().as_ref() != "except" {
-            return Err(XMLError::RngParseUnknownError);
-        }
-
-        Ok(Self {
-            name_class: Box::new(
-                value
-                    .first_child()
-                    .and_then(|ch| ch.as_element())
-                    .ok_or(XMLError::RngParseUnknownError)?
-                    .try_into()?,
-            ),
-        })
-    }
 }
 
 impl std::fmt::Display for RelaxNGExceptNameClass {
