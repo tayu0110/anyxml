e72d016bf2282c46f8cc0226ed5b2d6c4374ea27
tayu0110 <tayu.dev@gmail.com>
2026-03-05 11:24:34 +0900

    Add the `xml` crate as a benchmark target. However, it takes too long, so it is commented out.

--- benches/sax.rs
+++ benches/sax.rs
@@ -52,6 +52,29 @@ fn sax_53395538_fld_6697_l2_op_quick_xml(c: &mut Criterion) {
     });
 }
 
+// // reference: https://github.com/RazrFalcon/roxmltree/blob/master/benches/xml.rs
+// fn parse_via_xml_rs(path: &str) {
+//     let file = std::fs::File::open(path).unwrap();
+//     let mut config = xml::ParserConfig::new();
+//     config.ignore_comments = false;
+//     let reader = xml::EventReader::new(file);
+//     for event in reader {
+//         let _ = event.unwrap();
+//     }
+// }
+
+// fn sax_533946_luse_6668_2_op_xml_rs(c: &mut Criterion) {
+//     c.bench_function("xml-rs_533946_luse_6668_2_op.gml", |b| {
+//         b.iter(|| parse_via_xml_rs("benches/dataset/533946_luse_6668_2_op.gml"));
+//     });
+// }
+
+// fn sax_53395538_fld_6697_l2_op_xml_rs(c: &mut Criterion) {
+//     c.bench_function("xml-rs_53395538_fld_6697_l2_op.gml", |b| {
+//         b.iter(|| parse_via_xml_rs("benches/dataset/53395538_fld_6697_l2_op.gml"));
+//     });
+// }
+
 criterion_group!(
     anyxml,
     sax_533946_luse_6668_2_op,
@@ -62,4 +85,11 @@ criterion_group!(
     sax_533946_luse_6668_2_op_quick_xml,
     sax_53395538_fld_6697_l2_op_quick_xml
 );
+// `xml` crate is far too slow, so it has been excluded from the benchmark.
+// In v1.2.1, it took approximately 7000 seconds and 1200 seconds to complete each run.
+// criterion_group!(
+//     xml_rs,
+//     sax_533946_luse_6668_2_op_xml_rs,
+//     sax_53395538_fld_6697_l2_op_xml_rs
+// );
 criterion_main!(anyxml, quick_xml);
